abfb3d2b7a2bf6fabfdb1e30ed08dd75bb183ad2
XIuming Lee <lxmcoder@gmail.com>
2019-04-30 15:16:05 +0800

    再次测试

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
 (test) => {
-    let a = '本地修改';
+    let a = '本地修改再修改';
     console.log("本地修改");
-    console.log("github再次修改的");
 }
 
-//本地重新添加
-function del(本地重新添加){
-    console.log("本地重新添加");
-}
 //github重新增加的
 function add(重新增加的){
     console.log("重新增加的");
