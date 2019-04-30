a147de9fb668a5902201dc1c413cab2e37115197
XIuming Lee <lxmcoder@gmail.com>
2019-04-30 14:45:48 +0800

    测试1

--- test.js
+++ test.js
@@ -1,18 +1,13 @@
 (test) => {
     let a = '本地修改';
-    let d = 'd';
     let f = 'f';
-    console.log("本地修改");
-    console.log("测试2");
-    console.log("测试3");
-    console.log("测试4");
-    console.log("github修改的");
+    console.log("github修改的+本地修改");
     console.log("测试7");
 }
 
-//本地添加
-(本地添加) => {
-    console.log("本地添加");
+//本地重新添加
+function del(本地重新添加){
+    console.log("本地重新添加");
 }
 //github增加的
 (github增加的) => {
