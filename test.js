f960882acc1b4b2606113df5d8f8fc354b7f8c7a
XiumingLee <49981870+XiumingLee@users.noreply.github.com>
2019-04-30 11:35:47 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 (test) => {
-    let a = 'a';
-    let b = 'b';
+    let a = 'github修改的';
     let c = 'c';
     let d = 'd';
     let f = 'f';
-    console.log("测试1");
+    console.log("github修改的");
     console.log("测试2");
     console.log("测试3");
     console.log("测试4");
-    console.log("测试5");
     console.log("测试6");
     console.log("测试7");
+}
+
+//github增加的
+(github增加的) => {
+    console.log("github增加的");
 }
