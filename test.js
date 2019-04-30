1380d301f626809844dd8b9b50250764501d1889
XIuming Lee <lxmcoder@gmail.com>
2019-04-30 16:27:26 +0800

    branch001分支提交

--- test.js
+++ test.js
@@ -1,15 +1,14 @@
 (test) => {
-    let a = '本地修改再修改';
-    console.log("本地修改");
-    console.log("github再次修改的+++++");
+    let a = 'branch001';
+    console.log("branch001修改");
+    console.log("branch001修改的+++++");
 }
 
 //本地重新添加
-function del(本地重新添加--github在修改){
-    console.log("本地重新添加--github在修改");
+function del(branch001){
+    console.log("branch001");
 }
 //github重新增加的
 function add(重新增加的){
     console.log("重新增加的");
 }
-//github在加一条
