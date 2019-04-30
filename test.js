0cb9d3f1fcaae003a4e36ea7407a1aa1800c21a1
XIuming Lee <lxmcoder@gmail.com>
2019-04-30 16:22:22 +0800

    master分支提交

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
 (test) => {
     let a = '本地修改';
-    console.log("本地修改 --github在修改");
-    console.log("github再次修改的+++++");
+    console.log("master分支修改");
+    console.log("master分支修改");
 }
-
-//本地重新添加
-function del(本地重新添加--github在修改){
-    console.log("本地重新添加--github在修改");
-}
+//master分支的修改
 //github重新增加的
 function add(重新增加的){
     console.log("重新增加的");
