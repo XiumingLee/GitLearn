9b72a574da01b0e4ec4645ebba69c0b1670a66f7
XIuming Lee <lxmcoder@gmail.com>
2019-04-30 11:26:56 +0800

    test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+(test) => {
+    let a = 'a';
+    let b = 'b';
+    let c = 'c';
+    let d = 'd';
+    let f = 'f';
+    console.log("测试1");
+    console.log("测试2");
+    console.log("测试3");
+    console.log("测试4");
+    console.log("测试5");
+    console.log("测试6");
+    console.log("测试7");
+}
