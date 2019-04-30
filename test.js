3e1f442eb7082ac2ab816bcdb456a6d9c24298ba
XiumingLee <49981870+XiumingLee@users.noreply.github.com>
2019-04-30 14:40:46 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,14 @@
 (test) => {
     let a = '本地修改';
-    let d = 'd';
-    let f = 'f';
     console.log("本地修改");
-    console.log("测试2");
-    console.log("测试3");
-    console.log("测试4");
-    console.log("github修改的");
-    console.log("测试7");
+    console.log("github再次修改的");
 }
 
 //本地添加
 (本地添加) => {
     console.log("本地添加");
 }
-//github增加的
-(github增加的) => {
-    console.log("github增加的");
+//github重新增加的
+function add(重新增加的){
+    console.log("重新增加的");
 }
